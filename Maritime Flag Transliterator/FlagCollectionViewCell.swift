3fb41db51d87a0b5bc287e624a9b40a434d3df65
Austin Almond <austin226@live.com>
2016-01-11 13:55:28 -0500

    stuff

---
+++ Maritime Flag Transliterator/FlagCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlagCollectionViewCell.swift
+//  Maritime Flag Transliterator
+//
+//  Created by Austin Almond on 1/11/16.
+//  Copyright © 2016 Austin Almond. All rights reserved.
+//
+
+import UIKit
+
+class FlagCollectionViewCell: UICollectionViewCell {
+    
+}
